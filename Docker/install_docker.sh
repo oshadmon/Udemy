54b5be9d84a64e56d740cec95ad4d5543bc2a7bb
Ori Shadmon <oshadmon@gmail.com>
2021-03-14 04:34:09 +0000

    docker

--- Docker/install_docker.sh
+++ Docker/install_docker.sh
@@ -2,14 +2,11 @@ sudo apt-get -y update
 sudo apt-get -y upgrade 
 sudo apt-get -y update 
 
-sudo apt-get -y install docker.io
+sudo apt-get -y install docker.io docker-compose
 
 USER=`whoami`
 sudo groupadd docker
 sudo usermod -aG docker ${USER}
 newgrp docker
 
-
-
-
-
+sudo apt-get -y update
