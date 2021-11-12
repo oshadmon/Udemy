0afeee91669542045566469e43263c70dd330c46
Oi Shadmon <oshadmon@gmail.com>
2021-11-11 17:10:22 -0800

    polls up to show an exampple of getting status from 76.214.179.41:2051

--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+import requests
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    try:
+        r = requests.get('http://76.214.179.41:2051', headers={'command': 'get status', 'User-Agent': 'AnyLog/1.23' })
+    except Exception as e:
+        output = "Failed to execute GET status against '76.214.179.41:2051' (Error: %s)" % e
+    else:
+        if int(r.status_code) != 200:
+            output = "Failed to execute GET status against '76.214.179.41:2051' (Network Error: %s)" % r.status_code
+        else:
+            try:
+                output =  r.text
+            except Exception as e:
+                output = "Failed to extract GET status results from '76.214.179.41:2051' (Error: %s)" % e
+
+    return HttpResponse(output)
